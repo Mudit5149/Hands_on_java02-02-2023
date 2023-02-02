af1d238ffa7a2b8b5c2e9b1f58ec521518fd7bb4
Mudit5149 <86524837+Mudit5149@users.noreply.github.com>
2023-02-02 16:52:10 +0530

    Add files via upload

---
+++ Tryblock.java
@@ -0,0 +1,30 @@
+package access_Modifier;
+
+public class Tryblock {
+
+	public static void main(String[] args) {
+	
+
+		
+		        int a = 6000;
+		        int b = 0;
+		        // Without Try:
+//		        int c = a / b;
+//		        System.out.println("The result is " + c);
+		        // With Try:
+		        try {
+		            int c = a / b;
+		            System.out.println("The result is " + c);
+		        }
+		        catch(Exception e) {
+		            System.out.println("We failed to divide. Reason: ");
+		            System.out.println(e);
+		        }
+		        System.out.println("End of the program");
+		    }
+		
+
+
+	}
+
+
