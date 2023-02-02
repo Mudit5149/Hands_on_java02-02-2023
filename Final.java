9a2e3d2796b6adb16285354e5bd7451e3502a050
Mudit5149 <86524837+Mudit5149@users.noreply.github.com>
2023-02-02 14:40:16 +0530

    Add files via upload

---
+++ Final.java
@@ -0,0 +1,13 @@
+package access_Modifier;
+
+public class Final {
+
+	public static void main(String[] args) {
+		final int A=20;
+		System.out.println("the value of a is " +A);
+//		A=10;//we cannot reinitialize a variable inside the final method 
+//		System.out.println("the value of b" +A);
+
+	}
+
+}
